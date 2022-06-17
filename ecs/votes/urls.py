3e39269ce0ccb2199fb6790c96bce08a637e1b2a
halilbahar <halil.s.bahar@gmail.com>
2022-06-17 14:53:05 +0200

    Finish urls for votes

--- ecs/votes/urls.py
+++ ecs/votes/urls.py
@@ -5,11 +5,11 @@ from ecs.votes import views
 
 
 urlpatterns = (
-    path('<int:vote_pk>/download/', views.download_vote),
+    path('<int:vote_pk>/download/', views.download_vote, name='votes.download_vote'),
     path('<int:vote_pk>/sign', views.vote_sign),
 )
 
 if settings.DEBUG:
     urlpatterns += (
-        path('<int:vote_pk>/pdf/debug/', views.vote_pdf_debug),
+        path('<int:vote_pk>/pdf/debug/', views.vote_pdf_debug, name='votes.vote_pdf_debug'),
     )
