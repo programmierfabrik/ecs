7ebd1bedd897f0a825296dcff0a0a56d638cec64
halilbahar <halil.s.bahar@gmail.com>
2022-06-24 15:18:38 +0200

    Fix import error

--- ecs/core/forms/utils.py
+++ ecs/core/forms/utils.py
@@ -33,7 +33,9 @@ class ReadonlyFormSetMixin(object):
                 mark_readonly(form)
 
 def submission_form_to_dict(sf):
-    d = model_to_dict(sf)
+    d = model_to_dict(sf, exclude=['ethics_commissions', 'documents'])
+    d['ethics_commissions'] = list(sf.ethics_commissions.values_list('pk', flat=True))
+    d['documents'] = list(sf.documents.values_list('pk', flat=True))
     d['invoice_differs_from_sponsor'] = bool(sf.invoice_name)
     d['subject_females_childbearing'] = str((
         (True, True),
