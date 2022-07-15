1557293ea31f6e9b22fb3ca22cd1739557287ea1
halilbahar <halil.s.bahar@gmail.com>
2022-07-15 13:52:43 +0200

    Move ca, gpg and all other generated data into ./data

--- ecs/settings.py
+++ ecs/settings.py
@@ -217,7 +217,7 @@ LOGGING = {
 ##############
 
 # used by ecs.pki
-ECS_CA_ROOT = os.path.join(PROJECT_DIR, 'ca')
+ECS_CA_ROOT = os.path.join(PROJECT_DIR, 'data', 'ca')
 # if set to true:  users of internal groups need a client certificate to logon
 # ECS_REQUIRE_CLIENT_CERTS = false  # default
 
@@ -255,8 +255,8 @@ ECS_DOWNLOAD_CACHE_MAX_AGE = 30 * 24 * 60 * 60  # 30 days
 
 # Storage Vault settings
 STORAGE_VAULT = {
-    'dir': os.path.join(PROJECT_DIR, '..', 'ecs-storage-vault'),
-    'gpghome' : os.path.join(PROJECT_DIR, 'gpg'),
+    'dir': os.path.join(PROJECT_DIR, 'data', 'ecs-storage-vault'),
+    'gpghome' : os.path.join(PROJECT_DIR, 'data', 'gpg'),
     'encryption_uid': 'ecs_mediaserver',
     'signature_uid': 'ecs_authority',
 }
