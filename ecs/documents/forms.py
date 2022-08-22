ea40f19416e943b637d0828aadc5b8c86678d599
halilbahar <halil.s.bahar@gmail.com>
2022-08-22 15:18:24 +0200

    comment pdf sanitation out

--- ecs/documents/forms.py
+++ ecs/documents/forms.py
@@ -26,14 +26,15 @@ class DocumentForm(forms.ModelForm):
             raise ValidationError(_('This file is not a PDF document.'))
         pdf.seek(0)
         
-        # sanitization
-        try:
-            f = decrypt_pdf(pdf)
-        except ValueError:
-            raise ValidationError(_('The PDF-File seems to broken. For more Information click on the question mark in the sidebar.'))
+        # TODO: consider removing
+        # # sanitization
+        # try:
+        #     f = decrypt_pdf(pdf)
+        # except ValueError:
+        #     raise ValidationError(_('The PDF-File seems to broken. For more Information click on the question mark in the sidebar.'))
 
-        f.seek(0)
-        return UploadedFile(f, content_type='application/pdf', name='upload.pdf')
+        # f.seek(0)
+        return UploadedFile(pdf, content_type='application/pdf', name='upload.pdf')
 
     def clean(self):
         cd = super().clean()
