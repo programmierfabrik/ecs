6bf65321160fa6add45bb29f85b5e630492e8761
halilbahar <halil.s.bahar@gmail.com>
2022-10-04 11:22:20 +0200

    Be able to recieve mails with the new mail library

--- ecs/communication/management/commands/smtpd.py
+++ ecs/communication/management/commands/smtpd.py
@@ -1,28 +1,32 @@
 import logging
 
+from aiosmtpd.controller import Controller
 from django.core.management.base import BaseCommand
 
-from ecs.communication.smtpd import EcsMailReceiver
+from ecs import settings
+from ecs.communication.smtpd import SmtpdHandler
 
-
-log2level = {'DEBUG': logging.DEBUG, 'INFO': logging.INFO, 
-    'WARNING': logging.WARNING, 'ERROR': logging.ERROR,
-    'CRITICAL': logging.CRITICAL}
+log2level = {'DEBUG': logging.DEBUG, 'INFO': logging.INFO,
+             'WARNING': logging.WARNING, 'ERROR': logging.ERROR,
+             'CRITICAL': logging.CRITICAL}
 
 
 class Command(BaseCommand):
     help = 'Run receiving SMTP server.'
-    
+
     def add_arguments(self, parser):
         parser.add_argument('-l', '--loglevel', action='store',
-            choices=['debug', 'info', 'warning', 'error', 'critical'],
-            dest='loglevel', default='info', help='set loglevel'
-        )
+                            choices=['debug', 'info', 'warning', 'error', 'critical'],
+                            dest='loglevel', default='info', help='set loglevel'
+                            )
 
     def handle(self, **options):
         logging.basicConfig(
-            level = log2level[options['loglevel'].upper()],
-            format = '%(levelname)s %(message)s',
+            level=log2level[options['loglevel'].upper()],
+            format='%(levelname)s %(message)s',
         )
-        mail_receiver = EcsMailReceiver()
-        mail_receiver.run_loop()
+        hostname, port = settings.SMTPD_CONFIG['listen_addr']
+        controller = Controller(SmtpdHandler(), hostname=hostname, port=port)
+        controller.start()
+        input('SMTP server running. Press Return to stop server and exit.')
+        controller.stop()
