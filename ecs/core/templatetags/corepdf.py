70a2df35b5813f14a5aab52ce342d3f734f26128
halilbahar <halil.s.bahar@gmail.com>
2023-02-01 13:03:57 +0100

    Display proper age unit when rendering the core pdf

--- ecs/core/templatetags/corepdf.py
+++ ecs/core/templatetags/corepdf.py
@@ -5,7 +5,7 @@ from django.utils.safestring import mark_safe
 from django.utils.translation import gettext as _
 
 from ecs.core.models import AdvancedSettings
-
+from ecs.core.models.constants import SUBMISSION_AGE_UNIT
 
 register = Library()
 
@@ -13,11 +13,16 @@ register = Library()
 @register.filter
 def checkbox(val):
     return mark_safe('<span class="checkbox">%s</span>' % (val and "X" or " "))
+
+
 checkbox.is_safe = True
+
 
 @register.filter
 def yesno_checkboxes(val):
     return mark_safe('<span class="yesno">%s %s %s %s</span>' % (checkbox(val), _('yes'), checkbox(not val), _('no')))
+
+
 yesno_checkboxes.is_safe = True
 
 
@@ -28,6 +33,7 @@ class SingleCellTableNode(Node):
 
     def render(self, context):
         return '<table class="%s"><tr><td>%s</td></tr></table>' % (self.class_name, self.nodelist.render(context))
+
 
 #
 @register.tag
@@ -43,5 +49,9 @@ def print_logo_url():
     s = AdvancedSettings.objects.get()
     if not s.print_logo:
         return 'static:images/fallback_logo.png'
-    return 'data:{};base64,{}'.format(s.print_logo_mimetype,
-        base64.b64encode(s.print_logo).decode('ascii'))
+    return 'data:{};base64,{}'.format(s.print_logo_mimetype, base64.b64encode(s.print_logo).decode('ascii'))
+
+
+@register.filter
+def age_unit(unit):
+    return SUBMISSION_AGE_UNIT[unit - 1][1]
