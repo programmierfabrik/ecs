bd82e1026bc1e97ae4e41a2c22d77ec3d1c8e6d1
halilbahar <halil.s.bahar@gmail.com>
2023-03-13 08:52:17 +0100

    Implement search function for boilerplates by slug

--- ecs/boilerplate/forms.py
+++ ecs/boilerplate/forms.py
@@ -3,9 +3,16 @@ from django.utils.translation import gettext_lazy as _
 
 from ecs.boilerplate.models import Text
 
+
 class TextForm(forms.ModelForm):
     slug = forms.CharField(max_length=Text._meta.get_field('slug').max_length, label=_('Shorthand Symbol'))
 
     class Meta:
         model = Text
         fields = ('slug', 'text')
+
+
+class SearchBoilerplateForm(forms.Form):
+    slug = forms.CharField(max_length=Text._meta.get_field('slug').max_length,
+                           label=_('Shorthand Symbol'),
+                           required=False)
