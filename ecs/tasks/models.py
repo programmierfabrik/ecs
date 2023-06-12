9381b941a42c8b9f884383a93e4746aa7c8f6ff9
halilbahar <halil.s.bahar@gmail.com>
2023-06-12 12:28:14 +0200

    Revert "Make paper_submission_review visible for everything"
    This reverts commit 65ae7ef9e0aed147c16f3c68dc3ce0027e838858.

--- ecs/tasks/models.py
+++ ecs/tasks/models.py
@@ -67,6 +67,7 @@ class TaskQuerySet(models.QuerySet):
         return self.exclude(task_type__workflow_node__uid__in=(
             'resubmission',
             'b2_resubmission',
+            'paper_submission_review',
         ))
 
     def for_submissions(self, submissions):
